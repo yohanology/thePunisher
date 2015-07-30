6d5b0756b0e8c609967bc5dfd39d241d055045a1
Yohan <yohanology@gmail.com>
2015-07-31 01:55:06 +0800

    feat(v2): Added 1 missing punishment

--- scripts/thePunisher.js
+++ scripts/thePunisher.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 
-  var punishments = ["Buy coffee for everyone !", "Write a killer blog post !", "Suit up !", "End conversation with: Winter is coming !", "Sing in front of the class !", "Stand in the corner for 1 hour !", "Bow to everyone !", "Push up x2 for every minute late !"]
+  var punishments = ["Buy coffee for everyone !", "Write a killer blog post !", "Jog to Ferry Pier and take a selfie", "Suit up !", "End conversation with: Winter is coming !", "Sing in front of the class !", "Stand in the corner for 1 hour !", "Bow to everyone !", "Push up x2 for every minute late !"]
 
   var punish = function() {
     return punishments[Math.floor(Math.random() * punishments.length)];
