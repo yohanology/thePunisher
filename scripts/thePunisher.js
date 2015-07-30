7704b7fde0a18bcff89e426567ab9445566f4556
Yohan <yohanology@gmail.com>
2015-07-31 01:45:40 +0800

    feat(v2): Improved design according to Kill Bill style

--- scripts/thePunisher.js
+++ scripts/thePunisher.js
@@ -1,31 +1,16 @@
-var punishment = function(){
-    var punish = "";
-    punish = Math.floor(Math.random() * (9.5 - 1) + 1);
-    switch (punish) {
-        case 1:
-            return "Buy make coffee and serve everyone";
-        case 2:
-            return "Write a blog post";
-        case 3:
-            return "Come to class in former attire";
-        case 4:
-            return "End conversation with LUNCH IS COMING";
-        case 5:
-            return "Sing or dance in front of the class";
-        case 6:
-            return "Stand in the corner for 1 hour";
-        case 7:
-            return "Bow to everyone and apologize seriously";
-        case 8:
-            return "Push up x2 for every minute late";
-        case 9:
-            return "Jog to Ferry Pier during lunch and take a selfie (with a hot girl)";
-    }
-}
+$(document).ready(function(){
 
-var myButton = document.getElementById("punisher");
+  var punishments = ["Buy coffee for everyone !", "Write a killer blog post !", "Suit up !", "End conversation with: Winter is coming !", "Sing in front of the class !", "Stand in the corner for 1 hour !", "Bow to everyone !", "Push up x2 for every minute late !"]
 
-myButton.addEventListener("click", function() {
-    elem = document.getElementById("punishment");
-    elem.innerHTML = punishment();
-})
+  var punish = function() {
+    return punishments[Math.floor(Math.random() * punishments.length)];
+  }
+
+  $(document).on("click", "#punisher", function() {
+      $('#punishment').removeClass('animated rotateIn')
+      setTimeout(function(){
+        $('#punishment').addClass('animated rotateIn').html(punish());
+      },1)
+  });
+
+});
